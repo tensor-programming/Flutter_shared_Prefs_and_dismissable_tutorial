6b7069321ba08f890b3f34b6714f0f79d835c056
Tensor-Programming <abeltensor@tensor-programming.com>
2018-04-19 05:10:53 -0400

    Update rxdart-streamed-version.dart

--- lib/rxdart-streamed-version.dart
+++ lib/rxdart-streamed-version.dart
@@ -3,7 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:rxdart/rxdart.dart';
 import 'dart:async';
 
-//RXDart implementation is commented out, just remove the controllers around the published subject and it will work properly. 
+//RXDart implementation is commented out, just remove the comments around the published subject and it will work properly. 
 
 void main() => runApp(MyApp());
 
